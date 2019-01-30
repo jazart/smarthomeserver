06c03c92d0f1b88264b94e99b56d6b7c0a9da138
Daniel <cadetairmanda@yahoo.com>
2019-01-30 14:22:12 -0500

    Updated PlantContoller, graphpl, and User

--- src/main/kotlin/com/home/smarthomeserver/User.kt
+++ src/main/kotlin/com/home/smarthomeserver/User.kt
@@ -1,20 +1,27 @@
 package com.home.smarthomeserver
 
+import java.util.*
 import java.util.concurrent.ThreadLocalRandom
 
 open class Device{
+    open val info: DeviceInformation = DeviceInformation()
     open var name: String = ""
     open val id: String = ""
     open val status: Status = Status.OFF
 }
 
-data class User(val name: String = "",
-                val id: String = "",
-                val status: String = "")
+data class User(val name: String = "User Value goes here",
+                val id: String = UUID.randomUUID().toString(),
+                val info: DeviceInformation = DeviceInformation())
 
 data class PlantMoisture(val name: String = "",
                          val reading: MoistureReading
-                            = MoistureReading.Medium(ThreadLocalRandom.current().nextLong()))
+                         = MoistureReading.Medium(ThreadLocalRandom.current().nextLong()))
+
+
+data class DeviceInformation(val name: String = "device",
+                             val status: String = "--")
+
 
 //class Light() : Device {
 //    override var name: String
@@ -42,9 +49,6 @@ sealed class Temperature(val temp: Double) {
     class High(temp: Double) : Temperature(temp)
 
 }
-
-
-
 
 sealed class MoistureReading(readingLevel: Long) {
     data class High(val readingLevel: Long) : MoistureReading(readingLevel)
--- src/main/kotlin/com/home/smarthomeserver/controllers/PlantController.kt
+++ src/main/kotlin/com/home/smarthomeserver/controllers/PlantController.kt
@@ -1,15 +1,43 @@
 package com.home.smarthomeserver.controllers
 
-import com.home.smarthomeserver.MoistureReading
-import com.home.smarthomeserver.PlantMoisture
+import com.coxautodev.graphql.tools.GraphQLQueryResolver
+import com.home.smarthomeserver.*
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpHeaders
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
-@RestController class PlantController {
+@RestController
+class PlantController {
 
     @RequestMapping("/plant/{id}", method = [RequestMethod.GET])
     fun sendPlantStatus(@PathVariable("id") id: String) = PlantMoisture()
+}
+
+class DeviceResolver : GraphQLQueryResolver {
+    fun device() = Device()
+}
+
+class UserResolver : GraphQLQueryResolver {
+    fun user() = User()
+}
+
+class DeviceInformationResolver : GraphQLQueryResolver {
+    fun deviceInfo() = DeviceInformation()
+}
+
+@Configuration
+class GraphQLConfiguration {
+
+    @Bean
+    fun deviceResolver() = DeviceResolver()
+
+    @Bean
+    fun userResolver() = UserResolver()
+
+    @Bean
+    fun deviceInfoResolver() = DeviceInformationResolver()
 }
