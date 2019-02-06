4fae2e97c347b0cbfa0ec14da3dc2054f1bd237d
Daniel <cadetairmanda@yahoo.com>
2019-02-06 11:36:19 -0500

    Updated PlantContoller, graphpl, and User

--- src/main/kotlin/com/home/smarthomeserver/SmarthomeserverApplication.kt
+++ src/main/kotlin/com/home/smarthomeserver/SmarthomeserverApplication.kt
@@ -1,5 +1,6 @@
 package com.home.smarthomeserver
 
+import com.home.smarthomeserver.controllers.AwsBroker
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,7 @@ import org.springframework.boot.runApplication
 class SmarthomeserverApplication
 
 fun main(args: Array<String>) {
+
+    val broker = AwsBroker()
     runApplication<SmarthomeserverApplication>(*args)
 }
--- src/main/kotlin/com/home/smarthomeserver/controllers/AwsBroker.kt
+++ src/main/kotlin/com/home/smarthomeserver/controllers/AwsBroker.kt
@@ -1,14 +1,28 @@
 package com.home.smarthomeserver.controllers
 
-import com.amazonaws.services.iot.client.AWSIotConfig
-import com.amazonaws.services.iot.client.AWSIotMessage
+import com.amazonaws.services.iot.client.*
 import com.amazonaws.services.iot.client.AWSIotMqttClient
+
+
 
 class AwsBroker {
 
-//    init {
-//        val client = AWSIotMqttClient()
-//        val message = AWSIotMessage()
-//        val config = AWSIotConfig()
-//    }
+
+
+    init {
+        var clientEndpoint = "a81jmhvfdxdf-ats.iot.us-west-2.amazonaws.com"       // replace <prefix> and <region> with your own
+        var clientId = "test"                              // replace with your own client ID. Use unique client IDs for concurrent connections.
+        var awsAccesskey = "AKIAJZN5LIYOCOCFBIVQ"                       // X.509 based certificate file
+        var awsKey = "IdOmetPko51kjgYBw2ur0zbbzxg7kZPcUfB0EMGu"                        //
+        var client = AWSIotMqttClient(clientEndpoint, clientId, awsAccesskey, awsKey)
+        client.connect()
+        println("connection Sucessful ========================================success")
+        client.disconnect()
+        /* PKCS#1 or PKCS#8 PEM encoded private key file
+
+        // SampleUtil.java and its dependency PrivateKeyReader.java can be copied from the sample source code.
+        // Alternatively, you could load key store directly from a file - see the example included in this README.
+        var pair = SampleUtil.getKeyStorePasswordPair(certificateFile, privateKeyFile)
+        var client = AWSIotMqttClient(clientEndpoint, clientId, pair.keyStore, pair.keyPassword)*/
+    }
 }
