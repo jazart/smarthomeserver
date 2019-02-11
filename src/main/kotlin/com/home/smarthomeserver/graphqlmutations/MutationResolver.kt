dedc40f2f91b8ad78555f241b9d82478997f14c4
jazart <payne_jeremy@columbusstate.edu>
2019-02-11 11:02:25 -0500

    JP -Device command query

--- src/main/kotlin/com/home/smarthomeserver/graphqlmutations/MutationResolver.kt
+++ src/main/kotlin/com/home/smarthomeserver/graphqlmutations/MutationResolver.kt
@@ -1,7 +1,10 @@
 package com.home.smarthomeserver.graphqlmutations
 
 import com.coxautodev.graphql.tools.GraphQLMutationResolver
+import com.home.smarthomeserver.Status
 import com.home.smarthomeserver.User
+import com.home.smarthomeserver.controllers.DeviceController
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import java.util.*
 
@@ -9,10 +12,18 @@ import java.util.*
 @Component
 class MutationResolver : GraphQLMutationResolver {
 
+    @Autowired lateinit var controller : DeviceController
+
     fun signup(name: String, pass: String): User? =
             if (!(name.isBlank() && pass.isBlank())) {
                 User(name, UUID.randomUUID().toString())
             } else {
                 null
             }
+
+    fun update(uId: String, deviceName: String, command: Status): String{
+        controller.connect()
+        controller.updateDeviceStatus(uId, deviceName, command)
+        return "Ok"
+    }
 }
