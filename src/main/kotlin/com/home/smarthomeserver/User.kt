68f9245fee121356bb452e8003ad932463c68b03
jazart <payne_jeremy@columbusstate.edu>
2019-02-15 13:13:52 -0500

    JP -modified device and command queries

--- src/main/kotlin/com/home/smarthomeserver/User.kt
+++ src/main/kotlin/com/home/smarthomeserver/User.kt
@@ -3,12 +3,12 @@ package com.home.smarthomeserver
 import com.home.smarthomeserver.models.Command
 import java.util.*
 
-open class Device {
-    open val info: DeviceInformation = DeviceInformation()
-    open var name: String = ""
-    open val id: String = ""
-    open val status: Status = Status.DISCONNECTED
-}
+open class Device(
+        open val info: DeviceInformation = DeviceInformation(),
+        open var name: String = "",
+        open val id: String = "",
+        open val status: Status = Status.DISCONNECTED
+        )
 
 data class User(val name: String = "Jeremy",
                 val id: String = UUID.randomUUID().toString(),
@@ -22,9 +22,9 @@ data class DeviceInformation(val name: String = "device",
                              val status: String = "--",
                              val commands: List<Command> = listOf())
 
+
 data class Light(override var name: String = "Light",
-                 override val info: DeviceInformation = DeviceInformation(commands =
-                 listOf(Command.PULSE, Command.TURN_OFF, Command.TURN_ON))
+                 override val info: DeviceInformation = DeviceInformation(commands = listOf(Command.PULSE, Command.TURN_OFF, Command.TURN_ON))
 ) : Device()
 
 enum class Status {
