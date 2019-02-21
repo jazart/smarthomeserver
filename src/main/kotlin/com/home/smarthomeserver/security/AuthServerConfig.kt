f7503d368ddd131e8b1fb2186354e193171e15c1
jazart <payne_jeremy@columbusstate.edu>
2019-02-21 15:30:35 -0500

    JP -added oauth security for graphql access with login mutation open for unauthenticated users

---
+++ src/main/kotlin/com/home/smarthomeserver/security/AuthServerConfig.kt
@@ -0,0 +1,58 @@
+package com.home.smarthomeserver
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer
+import org.springframework.security.oauth2.provider.token.TokenStore
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore
+
+@Configuration
+@EnableAuthorizationServer
+class AuthServerConfig : AuthorizationServerConfigurerAdapter() {
+
+    @Autowired
+    lateinit var encryptor: BCryptPasswordEncoder
+
+    @Autowired
+    lateinit var tokenStore: TokenStore
+
+    @Autowired
+    lateinit var authManager: AuthenticationManager
+
+    override fun configure(server: AuthorizationServerSecurityConfigurer?) {
+        server?.run {
+            tokenKeyAccess("permitAll()")
+            checkTokenAccess("isAuthenticated()")
+        }
+    }
+
+    override fun configure(clients: ClientDetailsServiceConfigurer?) {
+        clients?.inMemory()
+                ?.withClient("TestId")
+                ?.authorizedGrantTypes("authorization_code", "refresh_token", "password")
+                ?.refreshTokenValiditySeconds(5_000_000)
+                ?.accessTokenValiditySeconds(500)
+                ?.authorities("USER")
+                ?.scopes("read", "write")
+                ?.autoApprove(true)
+                ?.secret(encryptor.encode("secret"))
+
+
+    }
+
+    override fun configure(endpoints: AuthorizationServerEndpointsConfigurer?) {
+        endpoints
+                ?.authenticationManager(authManager)
+                ?.tokenStore(tokenStore)
+    }
+
+    @Bean
+    fun tokenStore(): TokenStore = InMemoryTokenStore()
+}
