a76b7fc81de32544567d08d1dffa03dc8f08ccc2
kendrickgholston <kengholston8@gmail.com>
2019-03-03 20:57:35 -0500

    Domain to Entity & Added Device Repository

--- src/main/kotlin/com/home/smarthomeserver/UserRepository.kt
+++ src/main/kotlin/com/home/smarthomeserver/UserRepository.kt
@@ -3,22 +3,23 @@ package com.home.smarthomeserver
 import com.home.smarthomeserver.entity.ChildUserEntity
 import com.home.smarthomeserver.entity.ParentUserEntity
 import com.home.smarthomeserver.entity.User
+import com.home.smarthomeserver.models.ChildUser
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.NoRepositoryBean
 
 @NoRepositoryBean
 interface UserRepository<T : User> : JpaRepository<T, String> {
-    fun findUserByName(name: String): User?
-
+    fun findUserByUsername(username: String): User?
     fun existsUserByName(name: String): Boolean
 }
 
 interface ParentUserRepository : UserRepository<ParentUserEntity> {
     override fun existsUserByName(name: String): Boolean
-
-    override fun findUserByName(name: String): ParentUserEntity
-
+    override fun findUserByUsername(username: String): ParentUserEntity
 
 }
 
-interface ChildUserRepository : UserRepository<ChildUserEntity>
+interface ChildUserRepository : UserRepository<ChildUserEntity>{
+    override fun findUserByUsername(username: String): ChildUserEntity
+    override fun existsUserByName(name: String): Boolean
+}
